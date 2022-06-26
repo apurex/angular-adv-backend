9a95835b1e6700139c48709deea15a4341065d5c
Joel <taujomi17@gmail.com>
2022-06-26 01:57:03 -0400

    Seccion 12 - google

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const { googleVerify } = require("../helpers/google-verify");
 const { generarJWT } = require("../helpers/jwt");
 const Usuario = require("../models/usuario");
 
@@ -42,6 +43,49 @@ const login = async (req, res) => {
   }
 };
 
+const googleSignIn = async (req, res) => {
+  try {
+    const { email, name, picture } = await googleVerify(req.body.token);
+
+    const usuarioDB = await Usuario.findOne({ email });
+    let usuario;
+
+    if (!usuarioDB) {
+      usuario = new Usuario({
+        nombre: name,
+        email,
+        password: "@@@",
+        img: picture,
+        google: true,
+      });
+    } else {
+      usuario = usuarioDB;
+      usuario.google = true;
+    }
+
+    // generar json web token
+    await usuario.save();
+
+    // Generar el Token - JWT
+    const token = await generarJWT(usuario.id);
+
+    res.json({
+      ok: true,
+      email,
+      name,
+      picture,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      ok: false,
+      msg: "Token de Google no es correcto",
+    });
+  }
+};
+
 module.exports = {
   login,
+  googleSignIn,
 };
