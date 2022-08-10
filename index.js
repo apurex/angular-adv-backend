f0b6b8000237a81fcfe9ee1a9c9d25fceab6fef5
Joel <taujomi17@gmail.com>
2022-08-10 13:02:32 -0400

    SPA control

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const path = require("path");
 
 const { dbConnection } = require("./database/config");
 
@@ -26,6 +27,11 @@ app.use("/api/medicos", require("./routes/medicos"));
 app.use("/api/login", require("./routes/auth"));
 app.use("/api/todo", require("./routes/busquedas"));
 app.use("/api/uploads", require("./routes/uploads"));
+
+// Lo ultimo
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public/index.html"));
+});
 
 app.listen(process.env.PORT, () => {
   console.log("Servidor corriendo en el puesto " + process.env.PORT);
